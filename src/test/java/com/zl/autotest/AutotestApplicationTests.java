5906bbb4b2d236c834dd2ee4e35441b4887d6dd3
zl <wheesys@sina.com>
2019-05-06 10:13:06 +0800

    init

---
+++ src/test/java/com/zl/autotest/AutotestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zl.autotest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutotestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
